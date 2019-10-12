0468dfc9151712b337cc70feff533042b22e6e3d
Olya Bykovskaya <olya75825@gmail.com>
2019-10-12 22:45:12 +0300

    By points

--- Simple flight/ByPoints.py
+++ Simple flight/ByPoints.py
@@ -1,18 +1,48 @@
-#import rospy
-#from clever import srv
-#from std_srvs.srv import Trigger
+import rospy
+from clever import srv
+from std_srvs.srv import Trigger
 import sys #sys.argv
 import time
 from Point import Point
+from math 
+
+        
+
+rospy.init_node('square_flight')
+
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+navigate_global = rospy.ServiceProxy('navigate_global', srv.NavigateGlobal)
+set_position = rospy.ServiceProxy('set_position', srv.SetPosition)
+set_velocity = rospy.ServiceProxy('set_velocity', srv.SetVelocity)
+set_attitude = rospy.ServiceProxy('set_attitude', srv.SetAttitude)
+set_rates = rospy.ServiceProxy('set_rates', srv.SetRates)
+land = rospy.ServiceProxy('land', Trigger)
+
+def get_distance(x1, y1, z1, x2, y2, z2):
+    return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
+
+
+def navigate_wait(x, y, z, speed, frame_id, auto_arm):
+    tolerance = 0.2
+    print navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, auto_arm = auto_arm)
+
+    while not rospy.is_shutdown():
+        telem = get_telemetry(frame_id=frame_id)
+        print telem.z
+        if get_distance(x, y, z, telem.x, telem.y, telem.z) < tolerance:
+            break
+        rospy.sleep(0.2)
+        
 
 def get_points():
     user_args = sys.argv[1:]
     points_arr = []
     point = [0, 0, 0]
     counter = 0
+    cycles = 1
 
     for i in range(len(user_args)):
-
         flag = 1
         if flag:
             try:
@@ -25,62 +55,23 @@ def get_points():
             except:
                 flag = 0
                 continue
-    return points_arr
+        cycles = user_args[i]
 
+    print navigate(x=0, y=0, z=2, speed=0.2, frame_id="body", auto_arm = True)
+    rospy.sleep(7)
 
-a = get_points()
+    print "Take off"
+    tel = get_telemetry(frame_id="aruco_map")
+    base = Point(tel.x, tel.y, tel.z)
 
+    for i in cycles:
+        for point in points_arr:
+            print navigate_wait(x=point.x, y=point.y, z=point.z, speed=0.2, frame_id="aruco_map", auto_arm=False)
+            print "Passed"
 
-def nav(p):
-    time.sleep(1)
-    print p.x, p.y, p.z
+    navigate_wait(x=base.x, y=base.y, z=base.z, speed=0.2, frame_id="aruco_map", auto_arm=False)
+    print "Begin landing"
 
-
-start = Point(1, 2, 2)
-
-nav(Point(0, 0, 2))
-
-nav(start)
-
-cycles = 1
-
-for i in range(cycles):
-    for p in a:
-        nav(p)
-
-    nav(Point(0, 0, 2))
-
-
-##################################
-    
-def navigate(x=0, y=0, z=0, speed=0, frame_id=0):
-    print(x, y, z, speed, frame_id)
-        
-    
-def takeoff_wait(alt, speed=0.5, tolerance=0.2):
-    start = get_telemetry()
-    navigate(z=alt, speed=speed, frame_id='body', auto_arm=True)
-
-    while not rospy.is_shutdown():
-        if get_telemetry().z - start.z + z < tolerance:
-            break
-
-        rospy.sleep(0.2)
-
-
-def navigate_wait(x, y, z, speed, frame_id, tolerance=0.2):
-    start = get_telemetry()
-    #print navigate(z=alt, speed=speed, frame_id='body', auto_arm=True)
-
-    navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id)
-
-    while not rospy.is_shutdown():
-        telem = get_telemetry(frame_id=frame_id)
-        if get_distance(x, y, z, telem.x, telem.y, telem.z) < tolerance:
-            break
-        rospy.sleep(0.2)
-
-def land_wait():
-    land()
-    while get_telemetry().armed:
-        rospy.sleep(0.2)
+    res = land()
+    if res.success:
+        print "Perfect landing"
