23a3fef712951dc80b03e58d9bb666dc9f56ce6d
BeloborodovDS <dmitribeloborodov@yandex.ru>
2019-10-13 13:01:25 +0300

    Full direction control

--- Advanced flight/auto_fly_1.py
+++ Advanced flight/auto_fly_1.py
@@ -5,7 +5,7 @@ import sys
 import time
 import math
 
-from rpi_emg import EMGTelemetryQue, wait_for_command
+from rpi_emg import EMGTelemetryQue, wait_for_command, EMGTelemetry
 
 rospy.init_node('square_flight')
 
@@ -23,13 +23,13 @@ def get_distance(x1, y1, z1, x2, y2, z2):
     return math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2)
 
 
-def navigate_wait(x, y, z, speed, frame_id, auto_arm):
+def navigate_wait(x, y, z, speed, frame_id, auto_arm=False):
     tolerance = 0.2
     print navigate(x=x, y=y, z=z, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
 
     while not rospy.is_shutdown():
         telem = get_telemetry(frame_id=frame_id)
-        print telem.z
+        #print telem.z
         if get_distance(x, y, z, telem.x, telem.y, telem.z) < tolerance:
             break
         rospy.sleep(0.2)
@@ -50,37 +50,45 @@ telemetry = EMGTelemetryQue(delay=0.05)
 print('Waiting for takeoff')
 wait_for_command(telemetry, 'UP', 'UP')
 
-tolerance = 0.2
 print navigate(x=0, y=0, z=2, speed=0.2, frame_id="body", auto_arm=True)
-rospy.sleep(7)
 print "Take off"
+rospy.sleep(7)
+print "Take off finished"
 
 tel = get_telemetry(frame_id="aruco_map")
 base = Point(tel.x, tel.y, tel.z)
 
-def command(telemetry, pose1, pose2):
+def command(telemetry):
     data = telemetry.read()
-    telem = get_telemetry(frame_id='body')
+    print(data)
+    #telem = get_telemetry(frame_id='body')
     if (data.pose1, data.pose2) == ('SIDE', 'UP'):
-        navigate_wait(x=0.5, y=0, z=telem.z, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=1.5, y=0, z=0, speed=0.5, frame_id='body')
         print('right')
     elif (data.pose1, data.pose2) == ('SIDE', 'DOWN'):
-        navigate_wait(x=-0.5, y=0, z=telem.z, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=-1.5, y=0, z=0, speed=0.5, frame_id='body')
         print('left')
     elif (data.pose1, data.pose2) == ('UP', 'SIDE'):
-        navigate_wait(x=0, y=0.5, z=telem.z, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=0, y=1.5, z=0, speed=0.5, frame_id='body')
         print('forward')
     elif (data.pose1, data.pose2) == ('DOWN', 'SIDE'):
-        navigate_wait(x=0, y=-0.5, z=telem.z, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=0, y=-1.5, z=0, speed=0.5, frame_id='body')
         print('backward')
     elif (data.pose1, data.pose2) == ('UP', 'DOWN'):
-        navigate_wait(x=0, y=0, z=0.5, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=0, y=0, z=0.5, speed=0.5, frame_id='body')
         print('up')
     elif (data.pose1, data.pose2) == ('DOWN', 'UP'):
-        navigate_wait(x=0, y=0, z=-0.5, speed=0.5, frame_id='body')
+        print('start movement')
+        navigate(x=0, y=0, z=-0.5, speed=0.5, frame_id='body')
         print('down')
     elif (data.pose1, data.pose2) == ('UP', 'UP'):
-        navigate_wait(x=base.x, y=base.y, z=base.z, speed=0.2, frame_id="aruco_map", auto_arm=False)
+        print('start landing')
+        navigate(x=base.x, y=base.y, z=base.z, speed=0.2, frame_id="aruco_map", auto_arm=False)
         print "Begin landing"
         res = land()
         if res.success:
@@ -88,4 +96,5 @@ def command(telemetry, pose1, pose2):
 
 
 while True:
-    comand()
+    command(telemetry)
+    rospy.sleep(3)
